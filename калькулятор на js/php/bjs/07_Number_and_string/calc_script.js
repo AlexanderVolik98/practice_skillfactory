b55040c832e5e2806212cf144ae3bc8601325061
AlexanderVolik98 <55711112+AlexanderVolik98@users.noreply.github.com>
2020-08-30 20:08:10 +0300

    Add files via upload

---
+++ калькулятор на js/php/bjs/07_Number_and_string/calc_script.js
@@ -0,0 +1,146 @@
+let lastOperand = 0;
+let operation = null;
+
+const inputWindow = document.getElementById('inputWindow');
+
+
+// цифры 
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+document.getElementById('btn_2').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+document.getElementById('btn_3').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
+document.getElementById('btn_4').addEventListener('click', function () {
+    inputWindow.value += '4';
+})
+document.getElementById('btn_5').addEventListener('click', function () {
+    inputWindow.value += '5';
+})
+document.getElementById('btn_6').addEventListener('click', function () {
+    inputWindow.value += '6';
+})
+document.getElementById('btn_7').addEventListener('click', function () {
+    inputWindow.value += '7';
+})
+document.getElementById('btn_8').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+document.getElementById('btn_9').addEventListener('click', function () {
+    inputWindow.value += '9';
+})
+document.getElementById('btn_0').addEventListener('click', function () {
+    inputWindow.value += '0';
+})
+document.getElementById('btn_def').addEventListener('click', function () {
+    lastOperand = '-';
+})
+
+
+// цифры 
+
+
+// сумма
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+// сумма
+
+
+// разница
+document.getElementById('btn_def').addEventListener('click', function () {
+    
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'def';
+    inputWindow.value = '';
+
+})
+// разница
+
+// унарный минус
+
+// деление
+document.getElementById('btn_div').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'div';
+    inputWindow.value = '';
+})
+// деление
+
+
+//умножение
+document.getElementById('btn_mult').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'mult';
+    inputWindow.value = '';
+})
+// умножение
+
+
+// вычисление квадратного корня
+document.getElementById('btn_sqrt').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sqrt';
+    inputWindow.value = '';
+})
+
+// вычисление квадратного корня
+
+
+// унарный минус
+// var x = 3;
+// y = -x; // y = -3, x = 3
+
+// унарный минус
+
+
+// равенство (указание в operation операций над числами)
+document.getElementById('btn_calc').addEventListener('click', function () {
+    if (operation === 'sum') {
+        const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'def') {
+        const result = lastOperand - parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'div') {
+        const result = lastOperand / parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'mult') {
+        const result = lastOperand * parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'sqrt') {
+        const result = Math.sqrt(lastOperand);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+// равенство (указание в operation операций над числами)
+
+
+// clear_input
+document.getElementById('btn_clr').addEventListener('click', function () {
+    lastOperand = 0;
+    operation = null;
+    inputWindow.value = '';
+})
+// clear_input
+
+
