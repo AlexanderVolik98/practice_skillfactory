2ee86977b79f44f66490766992402f2793ef3c50
AlexanderVolik98 <55711112+AlexanderVolik98@users.noreply.github.com>
2021-05-12 23:18:17 +0300

    Delete protection.com directory

--- protection.com/App/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-define('ID', '7851659'); //ID приложения
-define('SECRET', 'OoD30IDMCy6dTq2BRkp1'); //защищенный ключ
-//define('serv_key_access', '452ddb65452ddb65452ddb65fd455a15ee4452d452ddb6525b7540fa3249b1cb7d3ceab'); //service key access
-define('URL', 'http://protection.com/vk.php');
